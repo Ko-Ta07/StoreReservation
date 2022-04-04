2b9cddf24e27f4c685ae0c32e95512946634e180
KOTA <sakamichi.hinatazaka.46@gmail.com>
2022-04-04 09:07:05 +0900

    second commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', include('app.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #if settings.DEBUG:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
